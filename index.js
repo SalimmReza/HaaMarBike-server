daf21b162c18aba2114f61f5080c89fc12a7a03d
SalimmReza <salimnuaa@gmail.com>
2022-11-24 01:38:13 +0600

    users added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.Port || 5000;
 
@@ -10,8 +10,43 @@ app.use(cors());
 app.use(express.json());
 
 
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.b9snwll.mongodb.net/?retryWrites=true&w=majority`;
+// console.log(uri);
+
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+
+        const usersCollection = client.db('assignment').collection('users');
+
+        // --------------------------------------------
+
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            console.log(user);
+            const result = await usersCollection.insertOne(user);
+            res.send(result);
+
+        })
+
+    }
+    finally {
+
+    }
+
+}
+run().catch(console.log.dir)
+
+
+
+
 app.use('/', (req, res) => {
-    res.send('assignment11');
+    res.send('assignment12');
 })
 
 app.listen(port, () => {
